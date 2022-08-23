8a713d291a2ddad29896eb15f666a1381b7bf3c6
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-08-23 22:30:07 +0200

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+listOfWords = {}
+
+while True :
+    print("Main menu: '1'-add word, '2'-search word, '3'-delete word, '4'-close ")
+    command = int(input("What do you want to do?: "))
+
+    if command == 1 :
+        key = str(input("Enter word: "))
+        definition = str(input("Enter definition of the word: "))
+
+        listOfWords.update({key: definition}) 
+        print("Word added succesfuly ")
+
+    elif command == 2 :
+        key = str(input("What word to serch?: "))
+        print(listOfWords[key])
+    
+    elif command == 3 :
+        key = str(input("What word you want to delete?: "))
+        del(listOfWords[key])
+        print("Word deleted succesful")
+
+    elif command == 4 :
+        break
+    
+    
