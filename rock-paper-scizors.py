58de5aed5715038f2f35afcc704a6e1dde3da572
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-03 09:36:37 +0200

    Update rock-paper-scizors.py

--- rock-paper-scizors.py
+++ rock-paper-scizors.py
@@ -5,25 +5,25 @@ opponent = 0
 
 a = ['paper', 'rock', 'scisors']
 
-'paper' > 'rock'
-'rock' > 'scisors'
-'scisors' > 'paper'
+"paper" > "rock"
+"rock" > "scisors"
+"scisors" > "paper"
 
 while (player < 3) and (opponent < 3):
 
     playerChoice = input("What u want to chose?: 'paper', 'rock', 'scisors'\n")
     opponentChoice  = random.choice(a)
 
-    if playerChoice == opponentChoice:
+    if (playerChoice == opponentChoice):
         print("opponent:", opponentChoice )
         print("draw")
 
-    elif playerChoice > opponentChoice:
+    elif (playerChoice < opponentChoice):
         print("opponent:", opponentChoice )
         player += 1
         print("You won point!", "You:", player, "Computer:", opponent )
         
-    elif opponentChoice > playerChoice:
+    elif (opponentChoice < playerChoice):
         print("opponent:", opponentChoice )
         opponent += 1
         print("You lost !", "You:", player, "Computer:", opponent )
