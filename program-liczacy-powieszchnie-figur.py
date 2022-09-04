8b5d416beca0135d743b04139a090c4eb61af3cc
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-04 13:25:35 +0200

    Update program-liczacy-powieszchnie-figur.py

--- program-liczacy-powieszchnie-figur.py
+++ program-liczacy-powieszchnie-figur.py
@@ -1,38 +1,36 @@
-import math
+import figury
+from enum import IntEnum 
 
-print("Welcome! This is program for counting the field of geometric figures.\n 1- kwadrat, 2-prostokąt, 3-koło, 4-trójkąt, 5-trapez, 6-wyjdź")
+Menu_Figury = IntEnum('Menu_figury', {'Kwadrat': 1, 'Prostokąt': 2, 'Koło': 3, 'Trójkąt': 4, 'Trapez': 5})
+
+print("Witam w programie liczącym pole figur!")
 
 while True:
-    wybor = int(input("What u want to do?: "))
+    wybor = int(input("""Wybierz figurę której pole chcesz obliczyć
+    1. Kwadrat
+    2. Prostokąt
+    3. Koło
+    4. Trójkąt
+    5. Trapez
+    """))
 
-    if wybor == 1:
-        a = int(input("Podaj dłg boku kwadratu: "))
-        def pole_kwadratu(a):
-            return a * a
-        print(pole_kwadratu(a))
-    elif wybor == 2:
-        a = int(input("Podaj dłg boku prostokąta: "))
-        b = int(input("Podaj dłg boku prostokąta: "))
-        def pole_prostokata(a, b):
-            return a * b
-        print(pole_prostokata(a, b))
-    elif wybor == 3:
+    if (wybor == Menu_Figury.Kwadrat):
+        a = float(input("Podaj dłg boku kwadratu: "))
+        print("Pole kwadratu wynosi: ", figury.pole_kwadratu(a))   
+    elif (wybor == Menu_Figury.Prostokąt):
+        a = float(input("Podaj dłg boku prostokąta: "))
+        b = float(input("Podaj dłg boku prostokąta: "))
+        print("Pole prostokąta wynosi: ", figury.pole_prostokata(a, b))
+    elif (wybor == Menu_Figury.Koło):
         r = int(input("Podaj dłg promienia koła: "))
-        def pole_kola(r):
-            return math.pi * r ** 2
-        print(pole_kola(r))
-    elif wybor == 4:
-        a = int(input("Podaj dłg podstawy trójkąta: "))
-        h = int(input("Podaj dłg wysokości trójkąta: "))
-        def pole_trojkata(a, h):
-            return 0.5 * a * h
-        print(pole_trojkata(a, h))
-    elif wybor == 5:
-        a = int(input("podaj dłg podstawy trapezu: "))
-        b = int(input("Podaj dłg podstawy trapezu: "))
-        h = int(input("POdaj dłg wysokości trapezu: "))
-        def pole_trapezu(a, b, h):
-            return (a + b) / 2 * h
-        print(pole_trapezu(a, b, h))
-    elif wybor == 6:
-        break 
+        print("Pole koła wynosi: ", figury.pole_kola(r))
+    elif (wybor == Menu_Figury.Trójkąt):
+        a = float(input("Podaj dłg podstawy trójkąta: "))
+        h = float(input("Podaj dłg wysokości trójkąta: "))
+        print("Pole trójkąta wynosi: ", figury.pole_trojkata(a, h))
+    elif (wybor == Menu_Figury.Trapez):
+        a = float(input("podaj dłg podstawy trapezu: "))
+        b = float(input("Podaj dłg podstawy trapezu: "))
+        h = float(input("POdaj dłg wysokości trapezu: "))
+        print("Pole trapezu wynosi: ", figury.pole_trapezu(a, b, h))
+    
