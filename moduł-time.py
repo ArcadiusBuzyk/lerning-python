2079406369a38e9b1910b129e0c32c0c265c865d
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-06 18:02:04 +0200

    Create moduł-time.py

---
+++ moduł-time.py
@@ -0,0 +1,42 @@
+import time
+
+#sumujDo1
+def sumuj_do(liczba):
+    suma = 0
+
+    for liczba in range (1, liczba+1):
+        suma = suma + liczba
+    return suma
+
+#sumujDo2
+def sumuj_do2(liczba):
+   return sum([liczba for liczba in range(1, liczba+1)])
+
+#sumujDo3
+def sumuj_do3(liczba):
+   return sum({liczba for liczba in range(1, liczba+1)})
+
+#sumujDo4
+def sumuj_do4(liczba):
+   return sum((liczba for liczba in range(1, liczba+1)))
+
+#sumujDo5
+def sumuj_do5(liczba):
+   return(1 + liczba) / 2 * liczba
+
+# przekazywanie funkcji 
+
+def function_performance (func, arg):
+    start = time.perf_counter()
+    func(arg)
+    end = time.perf_counter()
+    return end - start
+
+def show_message(message):
+    print(message) 
+
+print(function_performance(sumuj_do, 5000))
+print(function_performance(sumuj_do2, 5000))
+print(function_performance(sumuj_do3, 5000))
+print(function_performance(sumuj_do4, 5000))
+print(function_performance(sumuj_do5, 5000))
