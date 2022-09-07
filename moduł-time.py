84cb1662bb5d93a9fcc990ea34036bf99c7c30e5
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-07 14:14:40 +0200

    Update moduł-time.py

--- moduł-time.py
+++ moduł-time.py
@@ -26,16 +26,21 @@ def sumuj_do5(liczba):
 
 # przekazywanie funkcji 
 
-def function_performance (func, arg):
-    start = time.perf_counter()
-    func(arg)
-    end = time.perf_counter()
-    return end - start
+def function_performance (func, arg, how_many_times = 1):
+    sum = 0
+
+    for i in range (0, how_many_times):
+        start = time.perf_counter()
+        func(arg)
+        end = time.perf_counter()
+        sum = sum + (end - start)
+
+    return sum
 
 def show_message(message):
     print(message) 
 
-print(function_performance(sumuj_do, 5000))
+print(function_performance(sumuj_do, 5000, 25))
 print(function_performance(sumuj_do2, 5000))
 print(function_performance(sumuj_do3, 5000))
 print(function_performance(sumuj_do4, 5000))
