574a6c6971ea6318aed6b315bc0a1f1ce00a5943
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-17 00:37:11 +0200

    Update lotek.py

--- mini-games/lotek.py
+++ mini-games/lotek.py
@@ -3,8 +3,7 @@ import random
 def choose_random_numbers(amount, total_amount):
     numbersChoosen = []
     attempt = 0
-
-    while (attempt < 6) :
+    while (attempt < amount) :
         number = random.randint(0, total_amount)
 
         if number in numbersChoosen:
@@ -19,3 +18,8 @@ def choose_random_numbers(amount, total_amount):
 choose_random_numbers(6, 49)
 # 1
 
+def choose_random_numbers2(amount2, total_amount2):
+    print("Wylosowane liczby:", random.sample(range(total_amount2 + 1), amount2))
+
+choose_random_numbers2(6, 49)
+#2
