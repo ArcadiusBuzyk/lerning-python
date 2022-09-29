2a062c03dee7125d0ae920c61a0e7ce3cd4bc54e
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-29 12:37:11 +0200

    Update Argument-wielowartościowy.py

--- Argument-wielowartościowy.py
+++ Argument-wielowartościowy.py
@@ -1,9 +1,10 @@
 import time
 
-def function_performance (func, *arg, how_many_times = 1):
+
+def function_performance(func, *arg, how_many_times=1):
     sum = 0
 
-    for i in range (0, how_many_times):
+    for i in range(0, how_many_times):
         start = time.perf_counter()
         func(*arg)
         end = time.perf_counter()
@@ -11,16 +12,19 @@ def function_performance (func, *arg, how_many_times = 1):
 
     return sum
 
-setCointainer = {i for i in range(1000)}
-listCointainer =[i for i in range(1000)]
+
+setCointainer = [i for i in range(1000)]
+listCointainer = [i for i in range(1000)]
+
 
 def is_element_in(what_value, container):
-   if what_value in container:
-      return True
-   else:
-      return False
+
+    if what_value in container:
+        return True
+
+    else:
+        return False
 
 
-
-print(function_performance(is_element_in, 500, setCointainer, how_many_times= 1))
-print(function_performance(is_element_in, 500, listCointainer, how_many_times= 1))
+print(function_performance(is_element_in, 500, setCointainer, how_many_times=1))
+print(function_performance(is_element_in, 500, listCointainer, how_many_times=1))
