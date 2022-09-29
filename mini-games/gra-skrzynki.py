ec6d11cca4607d95a1c3c292927cdbdbded504ab
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-29 12:37:08 +0200

    Update gra-skrzynki.py

--- mini-games/gra-skrzynki.py
+++ mini-games/gra-skrzynki.py
@@ -21,8 +21,8 @@ for x in range(moves):
         drawChest = random.choices(list(chestType.keys()), list(chestType.values()))
         addNewChest = drawChestTotal.get(drawChest[0])
         drawChestTotal[drawChest[0]] = addNewChest + 1
- 
- 
+
+
 goldInDrawnChests = {
     key: drawChestTotal[key] * dropFromChest[key]
     for key in drawChestTotal
@@ -41,10 +41,10 @@ while (gameLenght > 0):
     gameAnswer = (input("Do you want to move forward?: "))
     chestSpawn = 60
     chestType = ['rare', 'very rare', 'epic', 'legendary']
-    
+
     if (gameAnswer == "yes" ):
         chanceOfChestSpawn = random.uniform(0, 100)
-       
+
         if (chestSpawn < chanceOfChestSpawn):
             chestYouDrop = random.choices(chestType, [75,20,4,1])
             if chestYouDrop == ['rare']:
@@ -57,31 +57,34 @@ while (gameLenght > 0):
                 goldSumary += 16000
             print("You find",chestYouDrop, "chest",)
             gameLenght -= 1
-        
+
         elif (chestSpawn > chanceOfChestSpawn):
             print("There isn't an chest!")
             gameLenght -= 1
-    else: 
+    else:
         print("You can only answer 'yes'")
         continue
-               
-    print("Your gold:", goldSumary)   
+
+    print("Your gold:", goldSumary)
 """
 from enum import Enum
+
 
 def findApproximateValue(value):
     lowestValue = value - 0.1 * value
     highestValue = value + 0.3 * value
     return random.randint(lowestValue, highestValue)
+
+
 gameLength = 5
 goldSumary = 0
 
-Event = Enum('Event',['Chest', 'Empty'])
+Event = Enum('Event', ['Chest', 'Empty'])
 
 eventDictionary = {
     Event.Chest: 0.6,
     Event.Empty: 0.4
-                  }
+}
 eventList = tuple(eventDictionary.keys())
 eventProbability = tuple(eventDictionary.values())
 
@@ -90,15 +93,15 @@ ChestType = Enum('ChestType', {
     'Epic': 'Epicka',
     'Mystical': 'Mistyczna',
     'legendary': 'legendarna'
-                         }
-                )
+}
+)
 
 chestTypeDictionary = {
     ChestType.Rare: 0.75,
     ChestType.Epic: 0.2,
     ChestType.Mystical: 0.04,
     ChestType.legendary: 0.01
-                      }
+}
 
 chestTypeList = tuple(chestTypeDictionary.keys())
 chestTypeProbability = tuple(chestTypeDictionary.values())
@@ -106,7 +109,7 @@ chestTypeProbability = tuple(chestTypeDictionary.values())
 rewardForChests = {
     chestTypeList[reward]: (reward + 1) * (reward + 1) * 1000
     for reward in range(len(chestTypeList))
-                 }
+}
 
 print("Welcome in game!")
 print("You have 5 steps to make, Lets find out how much gold u can collect.")
@@ -116,22 +119,22 @@ while (gameLength > 0):
     gameAnswer = input("Do you want to move forward?: ")
 
     if (gameAnswer == "yes"):
-        drawnEvent = random.choices(eventList,eventProbability)[0]
+        drawnEvent = random.choices(eventList, eventProbability)[0]
 
         if (drawnEvent == Event.Chest):
             print("You've drawn a Chest")
-            drawnChest = random.choices(chestTypeList,chestTypeProbability)[0]
+            drawnChest = random.choices(chestTypeList, chestTypeProbability)[0]
             print("You drawn", drawnChest.value, "chest")
             gamerReward = findApproximateValue(rewardForChests[drawnChest])
             goldSumary = goldSumary + gamerReward
             print(gamerReward)
             gameLength -= 1
 
-        elif(drawnEvent == Event.Empty):
+        elif (drawnEvent == Event.Empty):
             print("You've drawn nothing!")
             gameLength -= 1
-    
+
     else:
         print("You can only answer 'yes'")
         continue
-print("You have acquired: ",goldSumary, "Gold")
+print("You have acquired: ", goldSumary, "Gold")
