d43c13aabbdaff6489509aa5248c96cbb1bc7761
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-09-30 13:35:47 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,39 +1,36 @@
 listOfWords = {}
 
-while True :
+while True:
     print("Main menu: '1'-add word, '2'-search word, '3'-delete word, '4'-close ")
     command = int(input("What do you want to do?: "))
 
-    if command == 1 :
+    if command == 1:
         key = str(input("Enter word: "))
         if key in listOfWords:
             print("This word is already in dictionary")
         else:
             definition = str(input("Enter definition of the word: "))
 
-            listOfWords.update({key: definition}) 
+            listOfWords.update({key: definition})
             print("Word added successfuly ")
 
-    elif command == 2 :
+    elif command == 2:
         key = str(input("What word to serch?: "))
         if key in listOfWords:
             print(listOfWords[key])
         else:
             print("There is not such word in dictionary")
-    
-    elif command == 3 :
+
+    elif command == 3:
         key = str(input("What word you want to delete?: "))
         if key in listOfWords:
-            del(listOfWords[key])
+            del (listOfWords[key])
             print("Word deleted successful")
         else:
             print("There is not such word in dictionary")
 
-    elif command == 4 :
+    elif command == 4:
         print("You chose option: 'close'")
         break
     else:
         print("Wrong command! Try again.")
-
-    
-    
