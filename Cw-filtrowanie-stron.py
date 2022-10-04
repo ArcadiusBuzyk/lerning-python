10b537bd7c86a227785847fde9c4b63ef4b07413
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-04 17:17:46 +0200

    Update Cw-filtrowanie-stron.py

--- Cw-filtrowanie-stron.py
+++ Cw-filtrowanie-stron.py
@@ -1,8 +1,25 @@
-listOfNumbers = [1, 4, 20, 20, 45]
+import requests
+while True:
+    fileName = str(input("Podaj nazwę pliku z adresami stron lub zamknij: "))
 
-for number in listOfNumbers:
-    print(number)
+    if (fileName == "zamknij"):
+        break
 
+    else:
+        def filter_websides(path):
 
-def sample(a, b, c):
-    print(a)
+            with (open(path, 'r', encoding='UTF-8')) as file:
+                urlList = file.read().splitlines()
+                for address in range(len(urlList)):
+                    url = urlList[address]
+                    response = requests.get(url)
+                    if (response.status_code == 200):
+                        with (open('działajaceStrony.txt', 'a', encoding='UTF-8')) as file:
+                            file.write(url + '\n')
+                            print(url, "strona działa poprawnie")
+                    elif (response.status_code == 404):
+                        with (open('nieDziałajaceStrony.txt', 'a', encoding='UTF-8')) as file:
+                            file.write(url + '\n')
+                            print(url, "strona nie działa poprawnie")
+
+        fileContent = filter_websides(fileName)
