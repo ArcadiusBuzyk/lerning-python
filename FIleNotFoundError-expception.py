c4aa38653057964d4ac717d31b2cafdba055fe80
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-04 17:17:54 +0200

    Update FIleNotFoundError-expception.py

--- FIleNotFoundError-expception.py
+++ FIleNotFoundError-expception.py
@@ -1,5 +1,5 @@
 while True:
-    
+
     fileName = input("Podaj nazwę pliku lub zamknij: ")
     if (fileName == "zamknij"):
         break
@@ -8,11 +8,11 @@ while True:
         def file_Opener(path):
             try:
                 with open(path, "r", encoding="UTF-8")as file:
-                        return  print(file.read())
+                    return print(file.read())
 
             except FileNotFoundError:
-                print("Nie znaleziono pliku o takiej nazwie, podaj prawidłową nazwę pliku")
+                print(
+                    "Nie znaleziono pliku o takiej nazwie, podaj prawidłową nazwę pliku")
                 return
 
         fileContent = file_Opener(fileName)
-
