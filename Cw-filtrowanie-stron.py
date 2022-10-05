f8a461874f56acbe2bea989ffb031df90a2a523f
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-05 13:01:50 +0200

    Update Cw-filtrowanie-stron.py

--- Cw-filtrowanie-stron.py
+++ Cw-filtrowanie-stron.py
@@ -1,25 +1,21 @@
 import requests
-while True:
-    fileName = str(input("Podaj nazwę pliku z adresami stron lub zamknij: "))
 
-    if (fileName == "zamknij"):
-        break
+fileName = str(input("Podaj nazwę pliku z adresami stron lub zamknij: "))
 
-    else:
-        def filter_websides(path):
+with (open(fileName, 'r', encoding='UTF-8')) as file:
+    urlList = file.read().splitlines()
+    for address in range(len(urlList)):
+        url = urlList[address]
+        response = requests.get(url)
 
-            with (open(path, 'r', encoding='UTF-8')) as file:
-                urlList = file.read().splitlines()
-                for address in range(len(urlList)):
-                    url = urlList[address]
-                    response = requests.get(url)
-                    if (response.status_code == 200):
-                        with (open('działajaceStrony.txt', 'a', encoding='UTF-8')) as file:
-                            file.write(url + '\n')
-                            print(url, "strona działa poprawnie")
-                    elif (response.status_code == 404):
-                        with (open('nieDziałajaceStrony.txt', 'a', encoding='UTF-8')) as file:
-                            file.write(url + '\n')
-                            print(url, "strona nie działa poprawnie")
-
-        fileContent = filter_websides(fileName)
+        if (response.status_code == 200):
+            try:
+                with (open('działajaceStrony.txt', 'a+', encoding='UTF-8')) as file:
+                    file.write(url + '\n')
+                print(url, "strona działa poprawnie")
+            except ConnectionError:
+                continue
+        else:
+            with (open('nieDziałajaceStrony.txt', 'a+', encoding='UTF-8')) as file:
+                file.write(url + '\n')
+            print(url, "strona nie działa poprawnie")
