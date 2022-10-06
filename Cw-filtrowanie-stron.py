faba78c55d8f09ad71d374030302be2a9dd30ed6
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-06 13:25:22 +0200

    Update Cw-filtrowanie-stron.py

--- Cw-filtrowanie-stron.py
+++ Cw-filtrowanie-stron.py
@@ -1,21 +1,22 @@
 import requests
 
-fileName = str(input("Podaj nazwę pliku z adresami stron lub zamknij: "))
 
-with (open(fileName, 'r', encoding='UTF-8')) as file:
-    urlList = file.read().splitlines()
-    for address in range(len(urlList)):
-        url = urlList[address]
-        response = requests.get(url)
+def filter_web(path):
 
-        if (response.status_code == 200):
+    with (open(path, 'r', encoding='UTF-8')) as file:
+        urlList = file.read().splitlines()
+        for address in range(len(urlList)):
             try:
-                with (open('działajaceStrony.txt', 'a+', encoding='UTF-8')) as file:
-                    file.write(url + '\n')
-                print(url, "strona działa poprawnie")
-            except ConnectionError:
-                continue
-        else:
-            with (open('nieDziałajaceStrony.txt', 'a+', encoding='UTF-8')) as file:
-                file.write(url + '\n')
-            print(url, "strona nie działa poprawnie")
+                url = urlList[address]
+                response = requests.get(url)
+                if (response.status_code == 200):
+                    with (open('działajaceStrony.txt', 'a', encoding='UTF-8')) as file:
+                        file.write(url + "\n")
+            except requests.exceptions.ConnectionError:
+                with (open('nieDziałajaceStrony.txt', 'a', encoding='UTF-8')) as file:
+                    file.write(url + "\n")
+
+
+fileName = str(input("Podaj nazwę pliku: "))
+
+filter_web(fileName)
