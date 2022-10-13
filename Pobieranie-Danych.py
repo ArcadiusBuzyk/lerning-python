df4bfea0c819dec92332dd27d1a2d1d97ab2237f
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-13 18:35:35 +0200

    Update Pobieranie-Danych.py

--- Pobieranie-Danych.py
+++ Pobieranie-Danych.py
@@ -43,7 +43,7 @@ else:
     userWithTopCompletedTasks = get_users_with_top_completed_tasks(completedTaskFrequencyByUser)
     print(userWithTopCompletedTasks)
 
-
+"""
 # sposob 1
 r = requests.get("https://jsonplaceholder.typicode.com/users")
 users = r.json()
@@ -65,5 +65,30 @@ for userId in userWithTopCompletedTasks:
     r = requests.get("https://jsonplaceholder.typicode.com/users/" + str(userId))
     user = r.json()
     print("Wreczam ciasteczko osobie o imieniu: ", user["name"])
+"""
 
 # sposob 3
+
+
+def change_list_into_conj_of_param(my_list, key="id"):
+    conj_param = key + "="
+
+    lastIteration = len(my_list)
+    i = 0
+    for item in my_list:
+        i += 1
+        if (i == lastIteration):
+            conj_param += str(item)
+        else:
+            conj_param += str(item) + "&id="
+
+    return conj_param
+
+
+conj_param = change_list_into_conj_of_param(userWithTopCompletedTasks)
+
+r = requests.get("https://jsonplaceholder.typicode.com/users", params=conj_param)
+
+users = r.json()
+for user in users:
+    print("Wreczam ciasteczko osobie o imieniu: ", user["name"])
