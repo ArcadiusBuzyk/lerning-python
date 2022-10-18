cd0c8c2ce4996e178fd7e9b9b5bcc5309649b705
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-10-18 21:53:39 +0200

    Update dogsPictures.py

--- dogsPictures.py
+++ dogsPictures.py
@@ -2,10 +2,18 @@ import requests
 import json
 import webbrowser
 
-r = requests.get("")
+
+params = {
+    
+}
+
+r = requests.get("https://dog.ceo/api/breeds/list/all", params)
 
 try:
     content = r.json()
 except json.decoder.JSONDecodeError:
     print("Niepoprawny format")
 else:
+    dogBreeds = content["message"]
+    for dog in dogBreeds:
+        choice = input
