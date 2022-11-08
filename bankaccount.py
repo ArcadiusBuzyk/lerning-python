530cbb7714b83d01774cdccba17b3398a65536aa
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-11-08 21:49:23 +0100

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -1,7 +1,24 @@
-class User:
-    nextId = 1
+class Result:
+    def __init__(self, isSuccess, message, value):
+        self.isSuccess = isSuccess
+        self.message = message
+        self.value = value
 
-    def __init__(self, name=""):
-        self.name = name
-        self.id = User.nextId
-        User.nextId += 1
+
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def try_withdraw(self, amount):
+        if (self.balance >= amount):
+            self.balance -= amount
+            return Result(True, "Wypłacono kasę:", amount)
+
+        return Result(False, "Nie wypłacono kasy,"
+                      "Obecny stan konta:", self.balance)
+
+    def __str__(self):
+        return str(self.balance)
