857339b604ec87cacbfd658f35dc15447c2e3932
ArcadiusBuzyk <111864461+ArcadiusBuzyk@users.noreply.github.com>
2022-11-09 17:34:08 +0100

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -1,8 +1,20 @@
 class Result:
-    def __init__(self, isSuccess, message, value):
-        self.isSuccess = isSuccess
+    def __init__(self, message, value=None):
+        self.isSuccess = None
         self.message = message
         self.value = value
+
+
+class Ok(Result):
+    def __init__(self, message, value=None):
+        super().__init__(message, value)
+        self.isSuccess = True
+
+
+class Error(Result):
+    def __init__(self, message, value=None):
+        super().__init__(message, value)
+        self.isSuccess = False
 
 
 class BankAccount:
@@ -15,10 +27,26 @@ class BankAccount:
     def try_withdraw(self, amount):
         if (self.balance >= amount):
             self.balance -= amount
-            return Result(True, "Wypłacono kasę:", amount)
+            return Ok("Wypłacono kasę:", amount)
 
-        return Result(False, "Nie wypłacono kasy,"
-                      "Obecny stan konta:", self.balance)
+        return Error("Nie wypłacono kasy,"
+                     "Obecny stan konta:", self.balance)
 
     def __str__(self):
         return str(self.balance)
+
+
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self, balance=0, minimumBalance=1000):
+        super().__init__(balance)
+        self.minimumBalance = minimumBalance
+
+    def try_withdraw(self, amount):
+        if (self.balance - amount > self.minimumBalance):
+            return super().try_withdraw(amount)
+        else:
+            withdrawToMin = self.balance - self.minimumBalance
+            self.balance -= withdrawToMin
+            return Error("Przekroczono minimalny balans konta, "
+                         "wypłacamy pieniądze do minimalnej wartości. "
+                         "Stan konta:", self.balance)
